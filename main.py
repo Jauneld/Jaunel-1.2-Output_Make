bb9df982969287441638de68814fb0a99945a8aa
Jaunel Deans <16487860-JAUNELDEANS@users.noreply.replit.com>
2024-03-15 13:17:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Jaunel Deans
+# September 18, 2023
+#Task Make
+# Write a program that outputs a joke on multiple lines. 
+
+import time
+ # Sleep for 3 seconds
+print("1. What did the horse say after it tripped?")
+time.sleep(1.5)
+print("Answer: Help! I’ve fallen and I can’t giddyup!")
+time.sleep(3)
+print("2. What would bears be without bees?")
+time.sleep(1.5)
+print("Answer: Ears")
